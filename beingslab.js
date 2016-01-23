aae3e4518fa103955e0028e11662047f4cb09af0
cellier <shengbixing@gmail.com>
2016-01-23 11:19:17 +0800

    11
    11

---
+++ beingslab.js
@@ -0,0 +1,7 @@
+/**
+ * Created by maverick on 16/1/22.
+ */
+$("#title-delay").delay("200").fadeIn();
+$(".poster-img").delay("500").fadeIn();
+$(".text-main ").delay("800").fadeIn();
+$(".footer-text").delay("900").fadeIn();
---
+++ moblie/beingslab.js
@@ -0,0 +1,8 @@
+/**
+ * Created by maverick on 16/1/22.
+ */
+$("#title-delay").delay("200").fadeIn();
+$(".poster-img").delay("500").fadeIn();
+$(".text-main ").delay("800").fadeIn();
+$(".footer-text").delay("900").fadeIn();
+$(".contant-btn").delay("1000").fadeIn();
