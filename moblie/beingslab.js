9a5ec74cb1b05737bc80cc02c06e56a27db8c5f0
cellier <shengbixing@gmail.com>
2016-01-23 14:27:54 +0800

    yes lets go

--- moblie/beingslab.js
+++ moblie/beingslab.js
@@ -1,8 +1,8 @@
 /**
  * Created by maverick on 16/1/22.
  */
-$("#title-delay").delay("200").fadeIn(600);
-$(".poster-img").delay("600").fadeIn(600);
-$(".text-main ").delay("800").fadeIn(800);
-$(".footer-text").delay("2800").fadeIn(300);
-$(".contant-btn").delay("2000").fadeIn(600);
+$("#title-delay").delay("200").fadeIn(400);
+$(".poster-img").delay("800").fadeIn(400);
+$(".text-main ").delay("1000").fadeIn(800);
+$(".footer-text").delay("2000").fadeIn(300);
+$(".contant-btn").delay("1400").fadeIn(500);
